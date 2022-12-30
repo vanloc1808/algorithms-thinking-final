fa52cc9b1b46eb2580d9078c78383739af040e20
vanloc1808 <vanloc1808@gmail.com>
2022-12-30 11:24:10 +0700

    problem 12

---
+++ src/P12.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(int n) {
+    int a = -1, b = -1;
+    set<int> s;
+
+    // check if n is a sum of two squares
+    for (int i = 0; i <= n; i++) {
+        s.insert(i * i);
+
+        if (s.count(n - i * i)) {
+            a = i;
+            b = sqrt(n - i * i);
+            break;
+        }
+    }
+
+    if (a == -1 && b == -1) {
+        cout << "Impossible\n";
+    } else {
+        int x = min(a, b);
+        int y = max(a, b);
+
+        cout << x << " " << x << "\n";
+        cout << 2 * x << " " << x + y << "\n";
+        cout << 2 * x + y << " " << y << "\n";
+        cout << x + y << " " << 0 << "\n";
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    solve(n);
+}
